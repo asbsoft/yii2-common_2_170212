5caa083dbff400431ddfa071faf32b470304b96c
asbsoft <ab2014box@gmail.com>
2017-02-16 21:14:13 +0200

    some fixes

--- migrations/rbac/m160524_093501_addrole_to_root.php
+++ migrations/rbac/m160524_093501_addrole_to_root.php
@@ -4,19 +4,12 @@ use asb\yii2\web\UserIdentity;
 
 use yii\db\Migration;
 
+//Yii::setAlias('@asb/yii2/common_2_170212', '@vendor/asbsoft/yii2-common_2_170212'); // uncomment if need
+
 class m160524_093501_addrole_to_root extends Migration
 {
     protected $rootUserId = 90; //!! tune
     protected $roleRoot = 'roleRoot';
-
-    public function init()
-    {
-        parent::init();
-
-        //Yii::setAlias('@asb/yii2/common_2_170212', '@vendor/asbsoft/yii2-common_2_170212');
-        Yii::setAlias('@asb/yii2/cms_3_170211', '@vendor/asbsoft/yii2-cms_3_170211');
-        Yii::setAlias('@asb/yii2/modules', '@vendor/asbsoft/yii2module');
-    }
 
     public function safeUp()
     {
