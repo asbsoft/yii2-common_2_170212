777186f1c2ec567af1abaec7280d15283288e59a
asbsoft <ab2014box@gmail.com>
2017-02-22 21:00:56 +0200

    SluggableBehavior.php - add 'allowEmptySlug' feature to base Yii2-version

---
+++ behaviors/SluggableBehavior.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace asb\yii2\common_2_170212\behaviors;
+
+/**
+ * SluggableBehavior automatically fills the specified attribute with a value that can be used a slug in a URL.
+ *
+ * When attribute 'ensureUnique' is true, parent SluggableBehavior will generate for empty attribute slugs such as '-2'.
+ * To avoid this add attribute 'allowEmptySlug'.
+ *
+ * Usage:
+ * ```php
+ * public function behaviors()
+ * {
+ *     return [
+ *         [
+ *             'class' => SluggableBehavior::className(),
+ *             'attribute' => '...',
+ *             'ensureUnique' => true,
+ *             'allowEmptySlug' => true,
+ *         ],
+ *     ];
+ * }
+ * ```
+ * @author ASB <ab2014box@gmail.com>
+ */
+class SluggableBehavior extends \yii\behaviors\SluggableBehavior
+{
+    /** Set true to allow empty slug when ensureUnique property is true */
+    public $allowEmptySlug = false;
+
+    /**
+     * @inheritdoc
+     */
+    protected function makeUnique($slug)
+    {
+        if ($this->allowEmptySlug && empty($slug)) {
+            return '';
+        } else {
+            return parent::makeUnique($slug);
+        }
+    }
+
+}
