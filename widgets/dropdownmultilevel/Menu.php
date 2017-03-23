fe5805d90e0235d590f646d800cb6cdbce06fde5
asbsoft <ab2014box@gmail.com>
2017-03-23 12:46:38 +0200

    add comment to Menu.php

--- widgets/dropdownmultilevel/Menu.php
+++ widgets/dropdownmultilevel/Menu.php
@@ -4,6 +4,12 @@ namespace asb\yii2\common_2_170212\widgets\dropdownmultilevel;
 
 use yii\bootstrap\Nav;
 
+/**
+ * Renders a nav HTML component.
+ * Support multilevel dropdowns.
+ * For Yii2.0.10+ use in menu array 'dropDownOptions' => ['class' => 'dropdown-menu'] near 'items'.
+ * @see yii\bootstrap\Nav
+ */
 class Menu extends Nav
 {
     /**
