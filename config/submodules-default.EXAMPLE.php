f1e40d700b7b6cbf7cbc4e8d772fa6120880c757
asbsoft <ab2014box@gmail.com>
2017-04-08 22:28:41 +0300

    .

--- config/submodules-default.EXAMPLE.php
+++ config/submodules-default.EXAMPLE.php
@@ -5,6 +5,7 @@ $adminPath = empty(Yii::$app->params['adminPath']) ? 'admin' : Yii::$app->params
 return [ // module_uid => [config]
     'news' => [
         'class' => 'asb\yii2\modules\news_1_160430\Module',
+        //'layoutPath' => '@project/modules/sys/views/layouts',
         //'layoutPath' => '@asb/yii2/cms_3_170211/modules/sys/views/layouts',
         'routesConfig' => [ // type => prefix|array
             'main' => 'news',
