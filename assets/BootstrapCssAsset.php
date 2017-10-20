8b82fc5cb8050d7551bccc54e87a3d2ba2b7a3af
asbsoft <ab2014box@gmail.com>
2017-10-20 13:36:56 +0300

    use in assets $sourcePath = '@bower/...' instead of '@vendor/bower/...' because bower assets can be in different folders, for example /vendor/bower/ in Yii2-distributive, /vendor/bower-asset/ composer make by default

--- assets/BootstrapCssAsset.php
+++ assets/BootstrapCssAsset.php
@@ -6,8 +6,7 @@ use yii\web\AssetBundle;
 
 class BootstrapCssAsset extends AssetBundle
 {
-    //public $sourcePath = '@bower/bootstrap/dist';
-    public $sourcePath = '@vendor/bower/bootstrap/dist';
+    public $sourcePath = '@bower/bootstrap/dist';
     public $css = [
         'css/bootstrap.css',
     ];
