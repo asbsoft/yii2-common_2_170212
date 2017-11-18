a019caee3e45435bab4c3803c9ae35de1fa55916
asbsoft <ab2014box@gmail.com>
2017-11-18 13:47:31 +0200

    add default configs

---
+++ config/roles-default.php
@@ -0,0 +1,12 @@
+<?php
+
+return [ // userId => array(... roles list ...)
+    '90' => [
+        'roleRoot',
+    ],
+    '100' => [
+        'roleAdmin',
+    ],
+    '120' => [ // empty list means logined user ('@')
+    ],
+];
---
+++ config/users-default.php
@@ -0,0 +1,25 @@
+<?php
+
+return [ // userId => array(...)
+    '90' => [
+        'id' => '90',
+        'username' => 'root',
+        'password' => 'root12',
+        'authKey' => 'test090key',
+        'accessToken' => '090-token',
+    ],
+    '100' => [
+        'id' => '100',
+        'username' => 'admin',
+        'password' => 'adm123',
+        'authKey' => 'test100key',
+        'accessToken' => '100-token',
+    ],
+    '120' => [
+        'id' => '120',
+        'username' => 'demo',
+        'password' => 'demo12',
+        'authKey' => 'test101key',
+        'accessToken' => '101-token',
+    ],
+];
