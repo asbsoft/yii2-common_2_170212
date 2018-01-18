3af1053335104dd968ef5c616d97f35d842d9538
asbsoft <ab2014box@gmail.com>
2018-01-18 15:17:31 +0200

    cache config files

--- helpers/ConfigsBuilder.php
+++ helpers/ConfigsBuilder.php
@@ -2,6 +2,9 @@
 
 namespace asb\yii2\common_2_170212\helpers;
 
+use Yii;
+use yii\caching\Cache;
+
 /**
  * Module configs builder.
  *
@@ -9,4 +12,38 @@ namespace asb\yii2\common_2_170212\helpers;
  */
 class ConfigsBuilder extends BaseConfigsBuilder
 {
+    public static $defaultCacheDuration = 300; //sec
+
+    /** Cache key for saving configs */
+    public static $confFileCacheKey = 'confFileCacheKey';
+
+    /**
+     * @inheritdoc
+     * Use cache to save all configs together
+     */
+    public static function includeConfigFile($filename, $application = null)
+    {
+        if (empty($application)) {
+            $application = Yii::$app;
+        }
+        if (empty(static::$_configFiles) && $application->cache->exists(static::$confFileCacheKey)) {
+            static::$_configFiles = $application->cache->get(static::$confFileCacheKey);
+        }
+
+        return parent::includeConfigFile($filename, $application);
+    }
+
+    /**
+     * Save all configs together in cache
+     */
+    public static function cacheAllConfigsFile($application = null)
+    {
+        if (empty($application)) {
+            $application = Yii::$app;
+        }
+        if ($application->cache instanceof Cache && !empty(static::$_configFiles)) {
+            $application->cache->set(static::$confFileCacheKey, static::$_configFiles, static::$defaultCacheDuration);
+        }
+    }
+
 }
